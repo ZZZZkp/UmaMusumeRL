b67e11170f566ee8792112dd0ab193ac066a05da
ZZZZkp <pzy19960123@gmail.com>
2023-11-28 19:38:24 +0800

    add save, load function, and fix the init random action value for DQN.

--- DQN.py
+++ DQN.py
@@ -6,7 +6,7 @@ import numpy as np
 from tensorflow.python.keras import models, layers, optimizers
 import tensorflow as tf
 from typing import Any, List, Sequence, Tuple
-
+import datetime
 from game.game import Game
 
 
@@ -20,7 +20,7 @@ class DQN(object):
         self.target_model = self.create_model()
 
     def create_model(self):
-        """创建一个隐藏层为100的神经网络"""
+
         STATE_DIM, ACTION_DIM = 50, 7
         model = tf.keras.models.Sequential([
             tf.keras.layers.Dense(256, input_dim=STATE_DIM, activation='elu'),
@@ -41,17 +41,17 @@ class DQN(object):
         """预测动作"""
         # 刚开始时，加一点随机成分，产生更多的状态
         if np.random.uniform() < epsilon - self.step * 0.0002:
-            return np.random.choice([0, 1, 2])
+            return np.random.choice([0, 1, 2, 3, 4, 5, 6])
         return np.argmax(self.model.predict(np.array([s]), verbose=0)[0])
 
-    def save_model(self, file_path='UmaTrainModel-v0-dqn.h5'):
+    def save_model(self, file_path=f'UmaTrainModel-v0-dqn{datetime.datetime.now().strftime("%Y%m%d-%H%M%S")}.tf'):
         print('uma_model saved')
         self.model.save(file_path)
 
+    def load_model(self, file_path):
+        self.model = tf.keras.models.load_model(file_path)
+
     def remember(self, s, a, next_s, reward):
-        """历史记录，position >= 0.4时给额外的reward，快速收敛"""
-        if next_s[0] >= 0.4:
-            reward += 1
         self.replay_queue.append((s, a, next_s, reward))
 
     def train(self, batch_size=64, lr=1, factor=0.95):
@@ -98,6 +98,7 @@ env = Game()
 episodes = 1000  # 训练1000次
 score_list = []  # 记录所有分数
 agent = DQN()
+# agent.model = agent.load_model(file_path=)
 for i in range(episodes):
     s = env.reset()
     print(s.shape)
@@ -115,5 +116,6 @@ for i in range(episodes):
             break
     # 最后10次的平均分大于 12000 时，停止并保存模型
     if np.mean(score_list[-10:]) > 12000:
-        agent.save_model()
         break
+
+agent.save_model()
